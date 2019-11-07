8aaa75a17a0d817a3f6487b029b2e57fd82ca3d2
Yeo Tee Yang <silfer4560@yahoo.com.sg>
2019-11-08 01:28:32 +0800

    Solved B1

--- 2/B/App/start_project_2b1.py
+++ 2/B/App/start_project_2b1.py
@@ -2,15 +2,16 @@ import numpy as np
 import pandas
 import tensorflow as tf
 import csv
+import matplotlib.pylab as plt
 
 MAX_DOCUMENT_LENGTH = 100
 N_FILTERS = 10
-FILTER_SHAPE = [[20, 256],[20,1]] // original window size
+FILTER_SHAPE = [[20, 256],[20,1]] # original window size
 POOLING_WINDOW = 4
 POOLING_STRIDE = 2
 MAX_LABEL = 15
-
-no_epochs = 100
+batch_size = 128
+epochs = 10
 learning_rate= 0.01
 
 tf.logging.set_verbosity(tf.logging.ERROR)
@@ -20,25 +21,43 @@ tf.set_random_seed(seed)
 def char_cnn_model(x):
   
   input_layer = tf.reshape(
-      tf.one_hot(x, 256), [-1, MAX_DOCUMENT_LENGTH, 256, 1])
+      tf.one_hot(x, 256), [-1, MAX_DOCUMENT_LENGTH, 256, 1])  
 
   # CNN layer 1
-  conv1 = tf.layers.conv2d(
+  conv_1 = tf.layers.conv2d(
       input_layer,
       filters=N_FILTERS,
       kernel_size=FILTER_SHAPE[0],
       padding='VALID',
       activation=tf.nn.relu)
   # Pooling layer 1
-  pool1 = tf.layers.max_pooling2d(
-      conv1,
+  pool_1 = tf.layers.max_pooling2d(
+      conv_1,
       pool_size=POOLING_WINDOW,
       strides=POOLING_STRIDE,
       padding='SAME')
 
-  pool1 = tf.squeeze(tf.reduce_max(pool1, 1), squeeze_dims=[1])
+  # CNN layer 2
+  conv_2 = tf.layers.conv2d(
+      pool_1,
+      filters=N_FILTERS,
+      kernel_size=FILTER_SHAPE[1],
+      padding='VALID',
+      activation=tf.nn.relu)
+  # Pooling layer 1
+  pool_2 = tf.layers.max_pooling2d(
+      conv_2,
+      pool_size=POOLING_WINDOW,
+      strides=POOLING_STRIDE,
+      padding='SAME')
+  dim_sum_2 = pool_2.get_shape()[1].value * pool_2.get_shape()[2].value * pool_2.get_shape()[3].value
+  pool_2_flat = tf.reshape(pool_2, [-1, dim_sum_2])
+  #pool2 = tf.squeeze(tf.reduce_max(pool2, 1), squeeze_dims=[1])
 
-  layer_1_output = tf.layers.dense(pool1, MAX_LABEL, activation=None)
+  # Softmax Layer
+  W_softmax = tf.Variable(tf.truncated_normal([dim_sum_2, 15], stddev=1.0/np.sqrt(dim_sum_2)), name='weights_softmax')
+  b_softmax = tf.Variable(tf.zeros([15]), name='biases_3')
+  logits = tf.matmul(pool_2_flat, W_softmax) + b_softmax
 
 
   return input_layer, logits
@@ -48,13 +67,13 @@ def read_data_chars():
   
   x_train, y_train, x_test, y_test = [], [], [], []
 
-  with open('train_medium.csv', encoding='utf-8') as filex:
+  with open('../Data/train_medium.csv', encoding='utf-8') as filex:
     reader = csv.reader(filex)
     for row in reader:
       x_train.append(row[1])
       y_train.append(int(row[0]))
 
-  with open('test_medium.csv', encoding='utf-8') as filex:
+  with open('../Data/test_medium.csv', encoding='utf-8') as filex:
     reader = csv.reader(filex)
     for row in reader:
       x_test.append(row[1])
@@ -77,10 +96,10 @@ def read_data_chars():
   
 def main():
   
-  x_train, y_train, x_test, y_test = read_data_chars()
+  trainX, trainY, testX, testY = read_data_chars()
 
-  print(len(x_train))
-  print(len(x_test))
+  print(len(trainX))
+  print(len(testX))
 
   # Create the model
   x = tf.placeholder(tf.int64, [None, MAX_DOCUMENT_LENGTH])
@@ -90,21 +109,40 @@ def main():
 
   # Optimizer
   entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.one_hot(y_, MAX_LABEL), logits=logits))
-  train_op = tf.train.AdamOptimizer(lr).minimize(entropy)
+  train_op = tf.train.AdamOptimizer(learning_rate).minimize(entropy)
+
+  correct_prediction = tf.cast(tf.equal(tf.argmax(logits, 1), y_), tf.float32)
+  accuracy = tf.reduce_mean(correct_prediction)
 
   sess = tf.Session()
   sess.run(tf.global_variables_initializer())
 
   # training
-  loss = []
-  for e in range(no_epochs):
-    _, loss_  = sess.run([train_op, entropy], {x: x_train, y_: y_train})
-    loss.append(loss_)
+  train_accuracy,entropy_cost = [],[]
+  with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
 
+        for e in range(epochs):
+            N = len(trainX)
+            idx = np.arange(N)
+            np.random.shuffle(idx)
+            trainX, trainY = trainX[idx], trainY[idx] #shuffle
+            # Mini-batch training
+            for start, end in zip(range(0, len(trainX), batch_size), range(batch_size, len(trainX), batch_size)):
+                sess.run(train_op, {x: trainX[start:end], y_: trainY[start:end]})
+                
+            acc_,loss_ = sess.run([accuracy, entropy], {x: trainX, y_: trainY})
+            train_accuracy.append(acc_)
+            entropy_cost.append(loss_)
+            print('epoch', e, 'entropy', loss_,'accuracy', acc_)
 
-    if e%1 == 0:
-      print('iter: %d, entropy: %g'%(e, loss[e]))
-  
+        plt.figure()
+        plt.plot(range(epochs),train_accuracy,label="Training Accuracy")
+        plt.plot(range(epochs),entropy_cost,label="Entropy Cost")
+        plt.xlabel("Epochs")
+        plt.legend()
+        plt.savefig("../Out/B1.png")
+
   sess.close()
 
 if __name__ == '__main__':
